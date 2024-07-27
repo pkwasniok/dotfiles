0b85ff5f69fd094ae82a35fe487f15e58a519e3f
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2024-07-27 22:33:18 +0200

    Updated sway config & added waybar config

--- update.sh
+++ update.sh
@@ -8,18 +8,22 @@ cp -r -f ./zsh/.zshrc ~/
 echo 'Copied zsh configuration'
 
 # kitty
-cp -r -f ./kitty ~/.config/kitty
+cp -r -f ./kitty/* ~/.config/kitty/
 echo 'Copied kitty configuration'
 
 # neovim
-cp -r -f ./nvim ~/.config/nvim
+cp -r -f ./nvim/* ~/.config/nvim/
 echo 'Copied neovim configuration'
 
 # sway
-cp -r -f ./sway ~/.config/sway
+cp -r -f ./sway/* ~/.config/sway/
 echo 'Copied sway configuration'
 
+# waybar
+cp -r -f ./waybar/* ~/.config/waybar/
+echo 'Copied waybar configuration'
+
 # fonts
-cp -r -f ./fonts ~/.local/share/fonts
+cp -r -f ./fonts/* ~/.local/share/fonts/
 echo 'Copied fonts'
 
