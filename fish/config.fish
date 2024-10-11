be5071ce57a602a438c4b188bb620b6a3f1a7695
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2024-10-11 08:58:51 +0200

    Updated sway config

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,12 @@ set fish_vi_keybindings
 
 export EDITOR=nvim
 
-alias l=ls
-alias g=git
-alias vi=nvim
-alias btctl=bluetoothctl
+alias l="ls"
+alias g="git"
+alias vi="nvim"
+alias btctl="bluetoothctl"
 alias btconn="bluetoothctl devices Connected"
+alias vpnon="sudo wg-quick up protonvpn"
+alias vpnoff="sudo wg-quick down protonvpn"
+alias vpn="sudo wg"
 
