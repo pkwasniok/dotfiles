41bc45e23ee12901d6146a53a5c73f424b4f96d9
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2024-12-20 22:49:07 +0100

    Added harpoon to nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,21 @@ require("packer").startup(function (use)
     use("wbthomason/packer.nvim")
     use("catppuccin/nvim")
     use("nvim-treesitter/nvim-treesitter")
+
+    use("nvim-lua/plenary.nvim")
+    use({ "nvim-telescope/telescope.nvim", branch = '0.1.x' })
+    use({ "ThePrimeagen/harpoon", branch = "harpoon2" })
 end)
+
+-- Treesitter
+require("nvim-treesitter.configs").setup({
+    ensure_installed = { "lua", "c", "php" },
+    highlight = { enable = true },
+})
+
+-- Harpoon
+local harpoon = require("harpoon")
+harpoon:setup();
 
 -- Colorscheme
 vim.cmd.colorscheme("catppuccin-mocha")
@@ -46,9 +60,14 @@ vim.keymap.set({"t"}, "<ESC>", "<C-\\><C-N>")
 vim.keymap.set({"n", "v"}, "<C-j>", "<C-d>zz")
 vim.keymap.set({"n", "v"}, "<C-k>", "<C-u>zz")
 
--- Treesitter
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "lua", "c" },
-    highlight = { enable = true },
-})
+local builtin = require("telescope.builtin")
+vim.keymap.set({"n"}, "<leader>ff", builtin.find_files, { desc = 'Telescope find files' })
+
+vim.keymap.set({"n"}, "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set({"n"}, "<leader>ha", function() harpoon:list():add() end)
+vim.keymap.set({"n"}, "<C-u>", function() harpoon:list():select(1) end)
+vim.keymap.set({"n"}, "<C-i>", function() harpoon:list():select(2) end)
+vim.keymap.set({"n"}, "<C-o>", function() harpoon:list():select(3) end)
+vim.keymap.set({"n"}, "<C-p>", function() harpoon:list():select(4) end)
+
 
