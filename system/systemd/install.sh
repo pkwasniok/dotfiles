03e057a8497fca3469a21a8b6a63581262215572
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-02-28 20:19:14 +0100

    Added nftables

--- system/systemd/install.sh
+++ system/systemd/install.sh
@@ -4,6 +4,8 @@ systemctl enable --now bluetooth.service
 
 systemctl enable --now systemd-timesyncd.service
 
+systemctl enable --now nftables.service
+
 systemctl enable --now docker.service
 
 systemctl enable --now paccache.timer
