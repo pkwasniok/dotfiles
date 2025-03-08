ea746c22e7ec2cc79d6d253b5599d0611bacaa9b
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-03-08 12:27:58 +0100

    Added compressions tools

--- scripts/install/system/pacstrap.sh
+++ scripts/install/system/pacstrap.sh
@@ -4,6 +4,7 @@ pacstarp -K /mnt base linux linux-lts linux-firmware sof-firmware networkmanager
                  sudo efibootmgr btrfs-progs bluez-utils \
                  man-db man-pages neovim tmux fish curl htop rsync \
                  base-devel openssh git gdb make \
+                 bzip2 bzip3 gzip xz zstd 7zip tar zip unzip \
                  docker flatpak \
                  pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber \
                  sway swaybg waybar mako wofi wl-clipboard grim slurp libnotify noto-fonts ttf-font-awesome \
