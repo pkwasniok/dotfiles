2cb3a1bcbf56bac070830b116934070ab3815346
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-03-30 14:27:51 +0200

    Cleaned scripts directory

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir -p $HOME/.local/share/fonts
-cp -r Rubik $HOME/.local/share/fonts
-cp -r FantasqueSansMono $HOME/.local/share/fonts
+FONTS=$HOME/.local/share/fonts
+
+mkdir -p $FONTS
+
+cp -r Rubik $FONTS
+cp -r FantasqueSansMono $FONTS
 
--- system/systemd/install.sh
+++ system/systemd/install.sh
@@ -2,13 +2,13 @@ systemctl enable --now NetworkManager.service
 
 systemctl enable --now bluetooth.service
 
+systemctl enable --now reflector.service
+
+systemctl enable --now paccache.timer
+
 systemctl enable --now systemd-timesyncd.service
 
 systemctl enable --now iptables.service
 
 systemctl enable --now docker.service
-
-systemctl enable --now reflector.service
-
-systemctl enable --now paccache.timer
 
