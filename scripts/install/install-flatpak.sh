842a0bd2a1326488f742ff86889268b84abe6457
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2025-08-12 13:43:54 +0200

    add udev rules

--- scripts/install/install-flatpak.sh
+++ scripts/install/install-flatpak.sh
@@ -2,11 +2,15 @@ echo "=> Installing flatpak software"
 flatpak install org.gimp.GIMP \
                 org.inkscape.Inkscape \
                 org.kde.okular \
+                org.kde.dolphin \
                 org.blender.Blender \
                 org.freecad.FreeCAD \
                 org.kicad.KiCad \
                 org.signal.Signal \
                 org.telegram.desktop \
-                com.valvesoftware.Steam
                 com.discordapp.Discord
+                com.valvesoftware.Steam \
+                com.mikrotik.WinBox \
+                io.github.ebonjaeger.bluejay \
+                org.gnucash.GnuCash
 
